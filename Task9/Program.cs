cc995a4a386c98c1f813909d3b76ef8277847db2
Dinis <Dinis_abd@example.com>
2022-08-14 16:00:52 +0300

    Add variant 3

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -5,16 +5,46 @@
 // 12-> 2
 // 85 -> 8
 
+
+//Вариант 1:
+// int number = new Random().Next(10, 100);//10, 99 +1
+// Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+// else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
+
+
+
+//Вариант 2:
+// int number = new Random().Next(10, 100);//10, 99 +1
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// string result = firstDigit == secondDigit ? "Цифры равны"
+// : firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+// Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
+
+
+
+// Вариант 3:
 int number = new Random().Next(10, 100);//10, 99 +1
-//Cosole.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
 
 int firstDigit = number / 10;
 int secondDigit = number % 10;
 
-//if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
-//else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
-//else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
+int MaxDigit(int num)
+{
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+if (firstDigit == secondDigit) return 0;
+return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
 
-string result = firstDigit == secondDigit ? "Цифры равны"
-: firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
-Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
+int result = MaxDigit(number);
+string res = result == 0 ? "Цифры равны" : result.ToString();
+Console.WriteLine($"Наибольшая цифра числа {number} => {res}");
