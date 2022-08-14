155c699d125f2a441ed44987446657c750ebcba8
Dinis <Dinis_abd@example.com>
2022-08-14 16:58:13 +0300

    Add Task11,12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -5,3 +5,18 @@
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
 
+Console.Write("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2, не равное 0: ");
+int num2 = int.Parse(Console.ReadLine());
+int res = num1 % num2;
+if (res == 0)
+{
+    Console.WriteLine("Кратно ");
+}
+else 
+{
+    Console.WriteLine($"Не кратно, остаток -> {res}");
+}
+
+
