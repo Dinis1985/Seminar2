62a812369fca6e6b3aa0d650311108e34dbee80a
Dinis <Dinis_abd@example.com>
2022-08-15 12:11:47 +0300

    Add hw13

--- Hw13/Program.cs
+++ Hw13/Program.cs
@@ -7,57 +7,20 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int result = number;
+int num1 = number;
 
-if (number <= 100) Console.WriteLine($"Третья цифра числа {number} -> отсутствует ");
-else if (number >= 1000) Console.WriteLine($"Третья цифра числа {result / 10 % 10}");
-else if (number >= 10000) Console.WriteLine($"Третья цифра числа {(result / 100) % 10}");
-else if (number >= 100000) Console.WriteLine($"Третья цифра числа {(result / 1000) % 10}");
-else if (number >= 1000000) Console.WriteLine($"Третья цифра числа {result / 10000 % 10}");
-else if (number >= 10000000) Console.WriteLine($"Третья цифра числа {result / 100000 % 10}");
-else if (number >= 100000000) Console.WriteLine($"Третья цифра числа {result / 1000000 % 10}");
-else if (number >= 1000000000) Console.WriteLine($"Третья цифра числа {result / 10000000 % 10}");
-else Console.WriteLine($"Третья цифра числа {result % 10}");
-// {
-//     int result = number;
-//     for (; number >= 1000; number /= 10) Console.WriteLine($"Третья цифра числа {result % 10}");
-// } 
+if (num1 <= 100) Console.WriteLine($"Третья цифра числа {number} -> отсутствует ");
+else
+{
+    while (num1 >= 1000)
+    {
+        num1 = num1 / 10;
+    }
+    int res = num1 % 10;
+    Console.WriteLine($"Третья цифра числа {number} -> {res}");
+}
 
 
-// if (result < 0) Console.WriteLine($"Третья цифра числа {number} -> {result * -1}");
-// // else 
-// Console.WriteLine($"Третья цифра числа {number} -> {result} ");
-
-
-
-// if (N < 100) cout << "нету";
-// else
-// {
-//     for (; N >= 1000; N /= 10)
-//         ;
-//     ccut << N % 10;
-// }
-
-
-
-
-
-// if (number < 100)
-// {
-//     Console.WriteLine("Третья цифра числа {number} - отсутствует");
-// }
-// else if (result < 0) 
-// {
-//     Console.WriteLine($"Третья цифра числа {number} -> {result * -1}");
-// }
-// else if (result >= 1000) 
-// {
-//     Console.WriteLine($"Третья цифра числа {number} -> {result % 100}");
-// }
-// else
-// {
-//     Console.WriteLine($"Третья цифра числа {number} -> {result}");
-// }
 
 
 
